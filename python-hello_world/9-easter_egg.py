a9724c82d3ff732b98e6881c0749276efb97ec24
SibahleD <s.dlamini2@alustudent.com>
2024-06-26 02:37:20 -0700

    easter egg

---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+print(f"The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\n\
+Explicit is better than implicit.\nSimple is better than complex.\n\
+Complex is better than complicated.\nFlat is better than nested.\n\
+Sparse is better than dense.\nReadability counts.\n\
+Special cases aren't special enough to break the rules.\n\
+Although practicality beats purity.\n\
+Errors should never pass silently.\nUnless explicitly silenced.\n\
+In the face of ambiguity, refuse the temptation to guess.\n\
+There should be one-- and preferably only one --obvious way to do it.\
+\nAlthough that way may not be obvious at first unless \
+you're Dutch.\nNow is better than never.\nAlthough never is often \
+better than *right* now.\nIf the implementation is hard to explain, \
+it's a bad idea.\nIf the implementation is easy to explain, it may \
+be a good idea.\nNamespaces are one honking great idea -- let's do \
+more of those!")
