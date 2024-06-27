362e4c4cefa0271a0bc8cafab2353fad8c08127b
SibahleD <s.dlamini2@alustudent.com>
2024-06-27 04:15:56 -0700

    geo

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for tens in range(10):
     for ones in range(tens + 1, 10):
+        if not (tens * 10 + ones) == 89:
             print("{:d}{:d}, ".format(tens, ones), end="")
+        elif (tens * 10 + ones) == 89:
+            print("{:d}{:d}".format(tens, ones), end="\n")
