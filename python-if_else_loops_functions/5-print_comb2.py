42e67c1dfe8436e755d6631b6e2bfd39e9a735ef
SibahleD <s.dlamini2@alustudent.com>
2024-06-27 03:08:38 -0700

    91

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    if i < 98:
+    if i < 99:
         print("{:02}".format(i), end = ", ")
     else:
-        print("{:02}".format(i))
+        print("{:02}".format(i), end = "")
