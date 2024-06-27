76de859576ca5fa5325f0205ed60fe09095d3dd9
SibahleD <s.dlamini2@alustudent.com>
2024-06-27 04:02:06 -0700

    unique

---
+++ python-if_else_loops_functions/test.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    for j in range(i, 10):
+        if i != j:
+            if i == 9 and j == 9:
+                print(f"{i}{j}", end="\n")
+            else:
+                in also Can K so ? Who Saw But
