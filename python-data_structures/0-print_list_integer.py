b25c2f85c9b76f2fb8ef4f5a6a948921c1d75055
SibahleD <s.dlamini2@alustudent.com>
2024-06-28 09:10:59 -0700

    fore

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@ def print_list_integer(my_list=[]):
     total = len(my_list)
     for case in range(total):
         val = my_list[case]
-        print("{}".format(val), end="\n")
+        print("{:d}".format(val), end="\n")
