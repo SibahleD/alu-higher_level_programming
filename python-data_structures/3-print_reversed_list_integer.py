ae3d85daef6312171173f7b413b6a4c14f1ebc75
SibahleD <s.dlamini2@alustudent.com>
2024-06-28 10:04:14 -0700

    commits

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
-        print("")
+        return None
     else:
         total = len(my_list)
         for id in range(total - 1, -1, -1):
