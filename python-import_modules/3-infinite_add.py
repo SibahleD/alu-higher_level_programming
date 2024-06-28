6968db3734509f4480390a9f12de4225036dfac7
SibahleD <s.dlamini2@alustudent.com>
2024-06-28 06:01:21 -0700

    addition

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    values = argv[1:]
+    value = 0
+    arg_num = len(argv) - 1
+    if arg_num == 1 :
+        print("{}".format(values[0]), end="\n")
+    elif arg_num > 1:
+        for index in range(arg_num):
+            value = value + int(values[index])
+        print("{}".format(value), end="\n")
+    elif arg_num == 0:
+        print(0)
