dd7286e2f0b4649e75bf8aa75c184d185f4060b4
SibahleD <s.dlamini2@alustudent.com>
2024-06-28 01:41:06 -0700

    done

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uppercase(string):
-    # findng the length of the input
     leng = len(string)
     result = ""
-    # loop to check each character and convert to uppercase
     for char in range(leng):
-         #finding the ascii value of the character
         ascii_old = ord(string[char])
         if 97 <= ascii_old <= 122:
             ascii_new = ascii_old - 32
