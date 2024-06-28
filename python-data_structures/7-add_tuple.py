24948c75abb003b9dd4676a38adac6ad9b8929f5
SibahleD <s.dlamini2@alustudent.com>
2024-06-28 12:20:39 -0700

    tupless

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     res = []
-    for i in range(len(tuple_a)):
+    if len(tuple_a) > len(tuple_b):
+        total = len(tuple_a)
+    else:
+        total = len(tuple_b)
+    for i in range(total):
         if i == len(tuple_b):
             res.append(tuple_a[i])
         elif i > len(tuple_b):
