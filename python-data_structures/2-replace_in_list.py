0e26535dc604232b4b335edbb3492c1b29e4dce0
SibahleD <s.dlamini2@alustudent.com>
2024-06-28 09:38:51 -0700

    replacelist

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    total = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx >= total:
+        return my_list
+    else:
+        my_list[idx] = element
