a2c23894c2f5b970f08cc9269c6089eae0dd3488
SibahleD <s.dlamini2@alustudent.com>
2024-06-28 13:53:07 -0700

    for

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure both tuples have at least 2 elements by padding with zeros if necessary
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+
     res = []
-    if len(tuple_a) > len(tuple_b):
-        more = (tuple_a)
-        less = (tuple_b)
-    else:
-        less = (tuple_a)
-        more = (tuple_b)
     for i in range(2):
-        if i >= len(less):
-            res.append(more[i])
+        if len(tuple_a) <= i and len(tuple_b) <=i :
+            res.append(tuple_a[i] + tuple_b[i])
         else:
-            res.append(tuple_a[i]+tuple_b[i])
-    res = tuple(res)
-    return res
+            res.append(0)
+
+    return tuple(res)
