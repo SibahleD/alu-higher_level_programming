f4e5916e7af2a55c64a8db4fb98210d9732f016c
SibahleD <s.dlamini2@alustudent.com>
2024-06-29 01:54:03 -0700

    account 4 ()

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,10 +7,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         less = (tuple_a)
         more = (tuple_b)
-        if len(more) = 0:
+        if len(more) == 0:
             res.append(0)
             res.append(0)
-        elif len(more) = 1 and len(less) = 1:
+        elif len(more) == 1 and len(less) == 1:
             res.append(more[1]+less[1])
             res.append(0)
         else:
