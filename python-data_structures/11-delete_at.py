f540d29276c81ac125ed92857839266fc833df02
SibahleD <s.dlamini2@alustudent.com>
2024-06-29 02:44:49 -0700

    deleterr

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    total = len(my_list)
+    total = len(my_list) - 1
     if idx > len(my_list) or idx < 0:
         return my_list
     new_list = my_list
