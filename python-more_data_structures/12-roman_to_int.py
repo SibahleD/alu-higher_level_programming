52fcc6235a967e07e690ebd7fcc1e9b3a1fd380f
SibahleD <s.dlamini2@alustudent.com>
2024-06-29 07:50:57 -0700

    roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dig = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_dig = {
+            "I": 1, "V": 5, "X": 10,
+            "L": 50, "C": 100, "D": 500, "M": 1000
+    }
     int_val = 0
     fin_val = 0
     if isinstance(roman_string, str) == False:
