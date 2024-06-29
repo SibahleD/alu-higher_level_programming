9c78e70d3582c51f6f9c29cf0c6eeea5fed685c0
SibahleD <s.dlamini2@alustudent.com>
2024-06-29 01:42:40 -0700

    tuple index

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -8,8 +8,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
         less = (tuple_a)
         more = (tuple_b)
     for i in range(2):
-        if i >= len(less):
-            res.append(more[i])
+        if i > (len(less) - 1):
+            if not more[i]:
+                res.append(0)
+            else:
+                res.append(more[i])
         else:
             res.append(tuple_a[i]+tuple_b[i])
     res = tuple(res)
