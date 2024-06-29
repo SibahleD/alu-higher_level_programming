13e6d49d81278f9ef7ffd515a636f0d2e6bb3a25
SibahleD <s.dlamini2@alustudent.com>
2024-06-29 04:58:52 -0700

    search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,8 +3,8 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
         if my_list[i] == search:
-            new_row = list(map(lambda x: replace if  x== search else x, my_list))
+            new_row = list(map(lambda x: replace if x == search else x, my_list))        
             new_list.append(new_row)
         else:
-            new_list.append(new_list[i])
+            new_list.append(my_list[i])
         return new_list
