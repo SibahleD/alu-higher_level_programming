2f3aadbc450e8bd7bac08b078da2e5a424438090
SibahleD <s.dlamini2@alustudent.com>
2024-07-09 02:48:51 -0700

    int

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
+        except IndexError as e:
+            raise IndexError
             break
     print()
     return count
