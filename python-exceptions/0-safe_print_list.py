f7c1f6d694584920dc1f795908905bb1613bc53b
SibahleD <s.dlamini2@alustudent.com>
2024-07-09 01:36:56 -0700

    erors

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/in/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+        except Exception as e:
+            print("An unknown error occured: e")
+            break
+    print()
+    return count
