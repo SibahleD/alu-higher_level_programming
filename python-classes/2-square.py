45e86293b7b4140922b1541d766740ca29227dca
SibahleD <s.dlamini2@alustudent.com>
2024-07-10 09:14:46 -0700

    squaree

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -17,7 +17,7 @@ class Square:
         Raises a TypeError if size is less than 0
         """
         self.__size = size
-        if size < 0:
-            raise ValueError("size must be >= 0")
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
