531374bce86c3dec9f50f95717a3635675478a41
SibahleD <s.dlamini2@alustudent.com>
2024-07-10 08:11:42 -0700

    typeError

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """
-
+Instantiating a Square class
 """
 
 
 class Square:
     """
-    Init 
+    Create new class named Square
     """
     def __init__(self, size=0):
-        self.__size = size
+        """
+        Raises a TypeError if size is not an integer
+        Raises a ValueError if size is less than 0
+        """
+            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be >= 0")
+        if size < 0:
+            raise ValueError("size must be an integer")
