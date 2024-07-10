03af60915c1fe89cecacd432dba71603ff07f861
SibahleD <s.dlamini2@alustudent.com>
2024-07-10 08:19:03 -0700

    Size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,14 +7,17 @@ Instantiating a Square class
 class Square:
     """
     Create new class named Square
+
     """
     def __init__(self, size=0):
         """
-        Raises a TypeError if size is not an integer
-        Raises a ValueError if size is less than 0
+        Returns a value size (Default is 0)
+
+        Raises a ValueError if size is not an integer
+        Raises a TypeError if size is less than 0
         """
-            self.__size = size
+        self.__size = size
         if not isinstance(size, int):
-            raise TypeError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         if size < 0:
-            raise ValueError("size must be an integer")
+            raise TypeError("size must be an integer")
