91e78c48f8e57354bb7958c0dfa8e6cc96d38cd7
SibahleD <s.dlamini2@alustudent.com>
2024-07-10 11:23:40 -0700

    ff

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,6 +21,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     @property
     def size(self):
         return self.__size
