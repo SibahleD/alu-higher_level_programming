bc567d7429622e1016f80501d69a16161a160449
SibahleD <s.dlamini2@alustudent.com>
2024-07-12 12:39:56 -0700

    inherit

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+comparing instance of an object
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return true if obj is instance of a class"""
+    return isinstance(obj, a_class)
