38a3071c5fa40af14d0bc8c510301a9b99823e9b
SibahleD <s.dlamini2@alustudent.com>
2024-07-12 04:02:19 -0700

    gut

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,29 +15,31 @@ class Rectangle:
         """
         Returns a width and height value
         """
+        self._width = 0
+        self._height = 0
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        return self.width
+        return self._width
 
     @width.setter
-    def width(self, width):
-        if not isinstance(width, int):
+    def width(self, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        self.width = width
+        self._width = value
 
     @property
     def height(self):
-        return self.height
+        return self._height
 
     @height.setter
-    def height(self, height):
-        if not isinstance(height, int):
+    def height(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        self.height = height
+        self._height = value
