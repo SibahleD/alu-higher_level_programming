ecbfb96a20fe5f75832d414e039943375414391d
SibahleD <s.dlamini2@alustudent.com>
2024-07-12 16:57:10 -0700

    got

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,12 +2,14 @@
 """
 inheriting from 'list'
 """
+
+
 class MyList(list):
     """inherits from list"""
     def lookup(obj):
         """return the list"""
         return dir(obj)
-    
+
     def print_sorted(self):
         sorted_list = sorted(self)
         """"print the list in ascending order"""
