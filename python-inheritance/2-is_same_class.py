d34d3056c81f367567ebc009be4447779e62a393
SibahleD <s.dlamini2@alustudent.com>
2024-07-12 12:24:32 -0700

    module

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Finding the instance of an object
+Comparing the instance of an object to a class
 """
 def is_same_class(obj, a_class):
     """Return true if obj is instance of a class"""
