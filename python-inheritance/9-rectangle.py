5e44e9014a83e272b68e772710a49e93df94eab8
SibahleD <s.dlamini2@alustudent.com>
2024-07-12 15:17:02 -0700

    arena

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+importing methods from BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Arguments:
+        width 
+        height
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        result = f"[Rectangle] {self.__width}/{self.__height}"
+        return result
