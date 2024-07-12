1ec4fa281f34ee9a6ed86bf3e7f098d6d459842f
SibahleD <s.dlamini2@alustudent.com>
2024-07-12 15:53:14 -0700

    arena

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+importing methods from Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    New class Square imports methods from Rectangle
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        result = self.__size ** 2
+        return result
+
+    def __str__(self):
+        result = f"[Square] {self.__size}/{self.__size}"
+        return result
