accc3ee1107dcc6df140546547abfd4e9abbee97
SibahleD <s.dlamini2@alustudent.com>
2024-07-12 14:11:57 -0700

    forde

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,7 @@ comparing instance of an object
 
 
 def inherits_from(obj, a_class):
-    """Return true if obj is instance of a class"""
-    return isinstance(obj, a_class)
+    """
+    Return true if obj is instance of a class
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
