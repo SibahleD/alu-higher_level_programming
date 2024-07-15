87e24195d14a4294061634c9b17350fdb8647e2a
SibahleD <s.dlamini2@alustudent.com>
2024-07-15 03:55:31 -0700

    text

---
+++ python-input-output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+def read_file(filename=""):
+    """Reads  the textfile and returns a sting"""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data)
