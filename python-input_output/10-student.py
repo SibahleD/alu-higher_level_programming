e97baca1a04abbe3346316378a4fbfe13a6281ed
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 08:49:24 -0700

    self

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Returning dictionary into a data structure
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        if isinstance(attrs, list):
+            for name in attrs:
+                if isinstance(name, str) and name in self.__dict__:
+                    my_dict[name] = self.__dict__[name]
+            return my_dict
+        return self.__dict__
