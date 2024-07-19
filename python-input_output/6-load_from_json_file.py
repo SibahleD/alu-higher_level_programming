a77ad060f7edbe172468fde84900dda678fd2494
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 07:34:37 -0700

    got

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """
+making an object from a json file
 """
 
 import json
 
 def load_from_json_file(filename):
+    """
+    Makes an object from a json file
+    """
     with open(filename, 'r') as f:
         jsonobj = f.read()
         result == json.load(f)
