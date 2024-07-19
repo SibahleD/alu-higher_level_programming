1ff272407aa543ae83abf07e5e487db59d6464f6
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 07:53:41 -0700

    extend'

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Adds all arguments to a python list and saves to a json file
+"""
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+import sys
+import json
+
+
+if __name__ == "__main__":
+    filename = "add_item.json"
+
+    try:
+        items = load_from_json_file(filename)
+    except (FileNotFoundError, json.JSONDecodeError):
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
