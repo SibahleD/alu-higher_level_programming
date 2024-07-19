8f47d5b6b39b11957ae9bec884ca13b654fecadc
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 06:39:02 -0700

    json

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Decoding a file's json string representation
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Decoding a file's json string representation"""
+    jsonout = json.load(my_obj)
+    return jsonout
