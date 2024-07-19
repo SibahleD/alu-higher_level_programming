b073b9178bccab67371c1404df41b8b9f49461fa
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 02:24:11 -0700

    input

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,7 @@ Reading a text document
 
 
 def read_file(filename=""):
-    """Reads  the textfile and returns a sting"""
+    """Reads the textfile and returns a sting"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
