4d8200aa5efd8be15035d0c201df9ca05bc225e9
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 02:33:46 -0700

    mes

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,5 @@ def write_file(filename="", text=""):
     """writes the given text to a file with the specified filename"""
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
-    print(len(text))
+    chara = len(tex)
+    return chara
