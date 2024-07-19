066b181521933b09a23bf38a7230009c74236c06
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 07:15:11 -0700

    fir

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,13 @@
-#!/usr/python3
-"""Saves text to a textfile"""
+#!/usr/bin/python3
+"""Saves python oject to a json file"""
 
 
 import json
+
 def save_to_json_file(my_obj, filename):
+    """
+    Saves a python object to a file in json format
+    """
     with open(filename, 'w' ) as f:
         json_obj = json.dumps(my_obj)
         f.write(json_obj)
