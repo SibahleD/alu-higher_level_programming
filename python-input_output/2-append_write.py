ace53bd0986ecdd6040508c8c817d178dfe3299d
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 02:59:33 -0700

    append

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Adding text to a text file"""
 def append_write(filename="", text=""):
     """Appends given text to a file with the specified filename and prints the length of the text"""
-    with open(filename, 'w', encoding="utf-8") as f:
-        f.writelines(text)
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
     chara = len(text)
     return chara
