0295bfc71cf238c7bc028dfd7823b71f0f94ccda
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 07:55:09 -0700

    for

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,12 +3,12 @@
 Adds all arguments to a python list and saves to a json file
 """
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
 import sys
 import json
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
