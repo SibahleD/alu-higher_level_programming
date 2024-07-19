40d6d86ab82cfde23c0e5b6afe17e7a21fce3e92
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 02:26:44 -0700

    write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" """
+""" writing content into a text file"""
 
 
 def write_file(filename="", text=""):
+    """writes the given text to a file with the specified filename"""
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
