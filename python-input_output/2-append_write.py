83261cf7fb468c34347952d69f09311109ada30c
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 03:03:00 -0700

    line

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Adding text to a text file"""
+
+
 def append_write(filename="", text=""):
-    """Appends given text to a file with the specified filename and prints the length of the text"""
+    """Appends given text to a file with the specified filename
+    Prints the length of the text"""
     with open(filename, 'a', encoding="utf-8") as f:
         f.write(text)
     chara = len(text)
