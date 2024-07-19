15917c4e0924a4804bf45c97bc669f4e131ceffd
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 07:16:26 -0700

    white

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,10 +4,11 @@
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """
     Saves a python object to a file in json format
     """
-    with open(filename, 'w' ) as f:
+    with open(filename, 'w') as f:
         json_obj = json.dumps(my_obj)
         f.write(json_obj)
