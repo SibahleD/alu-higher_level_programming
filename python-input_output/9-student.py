e5545a9cb5361a71df58c38a0b2bd7d08031cfd0
SibahleD <s.dlamini2@alustudent.com>
2024-07-19 08:43:09 -0700

    la

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,9 +6,9 @@ Returning dictionary into a data structure
 
 class Student:
     def __init__(self, first_name, last_name, age):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         return self.__dict__
