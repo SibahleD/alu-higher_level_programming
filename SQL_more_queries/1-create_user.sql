2b0ec12e26bf649e395d85f6655faa96db8a4b14
SibahleD <s.dlamini2@alustudent.com>
2024-07-20 07:09:56 -0700

    ds

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- cratng a new user with al privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
