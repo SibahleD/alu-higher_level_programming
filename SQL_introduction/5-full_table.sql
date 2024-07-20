335d912d7b6e9292b762f133e3ea42bafcdc349a
SibahleD <s.dlamini2@alustudent.com>
2024-07-20 04:26:38 -0700

    h

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,14 @@
 -- Printing info on the first_table table
-INFO hbtn_0c_0 FOR first_table
+SELECT 
+    COLUMN_NAME AS 'Field', 
+    COLUMN_TYPE AS 'Type', 
+    IS_NULLABLE AS 'Null', 
+    COLUMN_KEY AS 'Key', 
+    COLUMN_DEFAULT AS 'Default', 
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'hbtn_0c_0' 
+    AND TABLE_NAME = 'first_table';
+
