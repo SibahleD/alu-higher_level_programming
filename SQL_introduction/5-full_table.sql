e1692df221c074f1ffbe7bf996028d78793a8477
SibahleD <s.dlamini2@alustudent.com>
2024-07-20 04:18:17 -0700

    f

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Printing info on the first_table table
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_0c_0';
+SHOW CREATE TABLE hbtn_0c_0.first_table;
