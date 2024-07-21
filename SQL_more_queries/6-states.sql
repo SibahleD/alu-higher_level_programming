cbb70ac66d0f038237a2a4d406b925e3a20d1558
SibahleD <s.dlamini2@alustudent.com>
2024-07-21 03:36:59 -0700

    gd

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- creating a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS states (
 	id INT NOT NULL UNIQUE PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
