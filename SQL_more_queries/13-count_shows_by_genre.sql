a11d38384b4a03f145164c5c599d865b0af0b9b2
SibahleD <s.dlamini2@alustudent.com>
2024-07-21 06:12:48 -0700

    ss

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lising number os shows with same genre
-SELECT genre, COUNT(tv_shows.id) as number_of_shows
+SELECT genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 GROUP BY tv_show_genres.genre
