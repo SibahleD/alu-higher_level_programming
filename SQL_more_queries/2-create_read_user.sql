865e16a3014531a18839a3df16cee6721afd36f9
SibahleD <s.dlamini2@alustudent.com>
2024-07-21 01:23:26 -0700

    d

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- creating a user with specific privileges
-
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on *.* TO 'user_0d_2'@'localhost'
+`
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
 
--- creating a new database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+-- Creating a new database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
