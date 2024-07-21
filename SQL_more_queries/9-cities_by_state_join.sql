1540dc113214feb8faf4dd61ce0cc46042a8cec2
SibahleD <s.dlamini2@alustudent.com>
2024-07-21 04:07:29 -0700

    dd

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Listing all cities by state
-SELECT cities.id, cities.name
-FROM cities, states
+SELECT cities.id, cities.name, states.name
+FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
