9f11ec7491caff464ec9bd38a85672e781688da6
SibahleD <s.dlamini2@alustudent.com>
2024-07-21 03:56:56 -0700

    ds

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Listing all cities by state
-USE hbbtn_0d_usa
+USE hbtn_0d_usa
 SELECT cities.id, cities.name, (states.name WHERE state.id = cities.id) AS state_name
 FROM cities, states
 ORDER BY cities.id ASC;
