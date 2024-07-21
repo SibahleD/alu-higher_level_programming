8fb1dba037c0fbddb99d84f137bc3e17486a341d
SibahleD <s.dlamini2@alustudent.com>
2024-07-21 06:12:09 -0700

    d

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lising number os shows with same genre
+SELECT genre, COUNT(tv_shows.id) as number_of_shows
+FROM tv_show_genres
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_show_genres.genre
+HAVING COUNT(tv_shows.id) > 0
+ORDER BY number_of_shows DESC
