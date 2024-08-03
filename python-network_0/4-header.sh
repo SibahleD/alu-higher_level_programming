13d9b600b2ae802ff2251db6eb4ba1f6f382595e
SibahleD <s.dlamini2@alustudent.com>
2024-08-03 10:21:08 +0000

    Soqa

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes URL, sends a GET request and displays the response body
-curl --header "X-HolbertonSchool-User-Id: 98" "$1"
+# takes URL, sends a GET request and displays the response body
+curl -sG -H "X-HolbertonSchool-User-Id: 98" "$1"
