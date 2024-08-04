78723ad10f53b448ea6094247f7ba6077df4f9ae
SibahleD <s.dlamini2@alustudent.com>
2024-08-04 14:17:50 +0000

    git

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL, sends a GET request and displays the response body
-curl -sG -H "X-HolbertonSchool-User-Id: 98" "$1"
+response=$(curl -sG -H "X-HolbertonSchool-User-Id: 98" "$1") && [ "$response" == "route validation with header parameter" ] && echo "$response"
