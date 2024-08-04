87e3149151c38ad23d46e517e59c2bb9d43bd7bc
SibahleD <s.dlamini2@alustudent.com>
2024-08-04 10:24:11 +0000

    Cole

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Fetches a https and displays response body
+from urllib import request
+with request.urlopen("https://alu-intranet.hbtn.io/status") as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
