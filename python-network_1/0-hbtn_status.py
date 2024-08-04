f7c7a7a6dec9d7822c2774c8257b47c54367d5ab
SibahleD <s.dlamini2@alustudent.com>
2024-08-04 12:23:02 +0000

    Dows

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ Fetches a https and displays response body """
-from urllib import request
-with request.urlopen("https://alu-intranet.hbtn.io/status") as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+import urllib.request
+url = "https://alu-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
