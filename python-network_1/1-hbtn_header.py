ae99edf162e0592403fe4806b7d158f44eeb6af4
SibahleD <s.dlamini2@alustudent.com>
2024-08-05 06:08:55 +0000

    fow

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ takes in a URL, sends a request to the URL and displays the
-variable value found in the resonse header """
+variable value found in the response header """
 import urllib.request
 from sys import argv
-with urllib.request.urlopen(argv[1]) as response:
-	html_id = response.info().get('X-Request-Id')
-    	print(html_id)
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        html_id = response.info().get('X-Request-Id')
+        print(html_id)
+
