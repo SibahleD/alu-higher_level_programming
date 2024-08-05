c3486c068f837c4a46b2b9dcc06404c94f564476
SibahleD <s.dlamini2@alustudent.com>
2024-08-05 05:47:37 +0000

    Sowa

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL, sends a GET request and displays the response body
-response=$(curl -sG -H "X-HolbertonSchool-User-Id: 98" "$1") && [ "$response" == "route validation with header parameter" ] && echo "$response"
+curl -s -H "X-School-User-Id: 98" "$1"
