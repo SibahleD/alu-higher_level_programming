b1bacbea1f6dee672e4867c7e388062e1ecee469
SibahleD <s.dlamini2@alustudent.com>
2024-08-05 06:01:38 +0000

    doe

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,7 +5,10 @@ variable value found in the resonse header
 """
 import urllib.request
 from sys import argv
-
-with urllib.request.urlopen(argv[1]) as response:
-    html_id = response.info().get('X-Request-Id')
-    print(html_id)
+if __name__ == '__main__':
+    """
+    takes in a URL, sends a request to the URL and displays the variable value found in the resonse header 
+    """
+    with urllib.request.urlopen(argv[1]) as response:
+        html_id = response.info().get('X-Request-Id')
+    	print(html_id)
