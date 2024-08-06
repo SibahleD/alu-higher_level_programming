3600d728ea6dd5dc726e800f40d384e2ddbb0d45
SibahleD <s.dlamini2@alustudent.com>
2024-08-06 17:16:01 +0000

    Dows

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """displays the response"""
+import requests
+
+
 if __name__ == "__main__":
-    import requests
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
