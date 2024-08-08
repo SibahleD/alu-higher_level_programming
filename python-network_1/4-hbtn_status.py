e771e6d36e5deb1157a4c0b100535e1b8aef4c78
SibahleD <s.dlamini2@alustudent.com>
2024-08-08 08:11:28 +0000

    status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """displays the response"""
 import requests
+
 if __name__ == "__main__":
-    import requests
     r = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
