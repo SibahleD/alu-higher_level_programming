2a81e91c9b823cc1b8737c223647ba00d8b6ef9f
SibahleD <s.dlamini2@alustudent.com>
2024-10-03 05:58:39 -0700

    java4

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+const content = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error making request:', error);
+    return;
+  }
+  const statusCode = response.statusCode;
+  console.log('Code:', statusCode);
+});
