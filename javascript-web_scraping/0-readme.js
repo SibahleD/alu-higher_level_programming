2d5db7aef7a81d0de7699a977c230d0893a88fe0
SibahleD <s.dlamini2@alustudent.com>
2024-10-03 04:49:08 -0700

    des

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error("Please provide a file path as the first argument.");
+  console.error('Please provide a file path as the first argument.');
   process.exit(1); // Exit with failure code
 }
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.error("Error reading file:", err);
+    console.error('Error reading file:', err);
   } else {
     console.log(data);
   }
