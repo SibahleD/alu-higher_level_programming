d797b706706039bdece1440f13da9350eea4fabb
SibahleD <s.dlamini2@alustudent.com>
2024-10-03 06:31:55 -0700

    java8

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
-const request = require('request');
-
-const filmId = 1;
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error making request:', error);
-    return;
-  }
-  const statusCode = response.statusCode;
-  
-  if (statusCode === 200) {
-    const film = JSON.parse(body);
-    console.log('${film.title}');
-  } else {
-    console.error('Request failed with status code:', statusCode);
-  }
+const req = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+req.get(url, (err, res) => {
+  err ? console.log(err) : console.log(JSON.parse(res.body).title);
 });
